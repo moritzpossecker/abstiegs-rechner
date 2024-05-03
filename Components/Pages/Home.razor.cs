530f21d137488b606c77dabe52ea5eadf1ed5906
Moritz P. <moritzpossecker@gmail.com>
2024-05-03 14:42:37 +0200

    Add design

--- Components/Pages/Home.razor.cs
+++ Components/Pages/Home.razor.cs
@@ -23,6 +23,9 @@ namespace Abstiegsrechner.Components.Pages
 
         private bool _dritteBrauchtAufstieg => relegationService.Calculator.ThirdTeamDependsOnPromotion;
 
+        private const string SUCCESS_CLASS = "success";
+        private const string DEPENDS_CLASS = "depending";
+        private const string FAIL_CLASS = "fail";
         protected override void OnInitialized()
         {
             base.OnInitialized();
@@ -33,6 +36,66 @@ namespace Abstiegsrechner.Components.Pages
         private async void UpdateUi()
         {
             await InvokeAsync(StateHasChanged);
+        }
+
+        private string GetFirstTeamCardClass()
+        {
+            if (_ersteSteigtAb)
+                return FAIL_CLASS;
+
+            return SUCCESS_CLASS;
+        }
+
+        private string GetSecondTeamCardClass()
+        {
+            if (!_zwoteSteigtAb)
+                return SUCCESS_CLASS;
+
+            if (_zwoteBrauchtAufstieg)
+                return DEPENDS_CLASS;
+
+            return FAIL_CLASS;
+        }
+
+
+        private string GetThirdTeamCardClass()
+        {
+            if (!_dritteSteigtAb)
+                return SUCCESS_CLASS;
+
+            if (_dritteBrauchtAufstieg)
+                return DEPENDS_CLASS;
+
+            return FAIL_CLASS;
+        }
+
+        private string GetSecondTeamDescription()
+        {
+            if(!_zwoteSteigtAb)
+                return string.Empty;
+
+            if (!_zwoteBrauchtAufstieg)
+                return string.Empty;
+
+            return "Können noch die Klasse halten, wenn der Stadtligameister die Relegation gewinnt.";
+        }
+
+        private string GetThirdTeamDescription()
+        {
+            if (!_dritteSteigtAb)
+                return string.Empty;
+
+            if(_dritteSteigtOhneZwoteAb)
+                if (_dritteBrauchtAufstieg)
+                {
+                    return "Können noch die Klasse halten, wenn der Stadtligameister die Relegation gewinnt.";
+                }
+                else
+                {
+                    return string.Empty;
+                }
+
+            return "Steigen durch Zwangsabstieg ab, weil die Zwote absteigt.";
         }
     }
 }
